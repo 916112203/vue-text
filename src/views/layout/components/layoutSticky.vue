668dcf8ac4222bef78c4e9811f256e22c7f2cd1f
zcj <916112203@qq.com>
2023-11-09 12:10:07 +0800

    pinia优化导航

--- src/views/layout/components/layoutSticky.vue
+++ src/views/layout/components/layoutSticky.vue
@@ -1,11 +1,18 @@
 <script setup>
-import { useScroll } from '@vueuse/core'
+import { useScroll } from "@vueuse/core";
 
-const { y } = useScroll(window)
+import { useCategoryStore } from "@/stores/category";
+
+const categoryStore = useCategoryStore()
+
+
+
+
+const { y } = useScroll(window);
 </script>
 
 <template>
-	<div class="app-header-sticky" :class="{show:y>78}">
+	<div class="app-header-sticky" :class="{ show: y > 78 }">
 		<div class="container">
 			<RouterLink class="logo" to="/" />
 			<!-- 导航区域 -->
@@ -13,32 +20,8 @@ const { y } = useScroll(window)
 				<li class="home">
 					<RouterLink to="/">首页</RouterLink>
 				</li>
-				<li>
-					<RouterLink to="/">居家</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">美食</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">服饰</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">母婴</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">个护</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">严选</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">数码</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">运动</RouterLink>
-				</li>
-				<li>
-					<RouterLink to="/">杂项</RouterLink>
+				<li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
+					<RouterLink to="/">{{ item.name }}</RouterLink>
 				</li>
 			</ul>
 
