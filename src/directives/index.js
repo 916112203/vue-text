9428edb435da6fa0beded9d8a8fff264e6a6be6a
zcj <916112203@qq.com>
2023-11-10 16:44:18 +0800

    图片懒加载及优化

---
+++ src/directives/index.js
@@ -0,0 +1,20 @@
+import { useIntersectionObserver } from "@vueuse/core";
+
+export const lazyPlugin = {
+	install(app) {
+		app.directive("img-lazy", {
+			mounted(el, binding) {
+				//el：指令绑定的元素
+				//binding：binding.value 指令等于号后面绑定的表达式的值
+				console.log(el, binding.value);
+				const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+					console.log(isIntersecting);
+					if (isIntersecting) {
+						el.src = binding.value;
+                        stop()
+					}
+				});
+			},
+		});
+	},
+};
--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import router from './router'
 
 import { getCategory } from '@/apis/testAPI'
 
-import { useIntersectionObserver } from "@vueuse/core";
+import { lazyPlugin } from '@/directives/index'
 // axios测试
 getCategory().then(res => {
     console.log(res);
@@ -19,26 +19,10 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(lazyPlugin);
 
 app.mount('#app')
 
 
 //定义全局指令
 
-app.directive('img-lazy',{
-    mounted(el,binding) {
-        //el：指令绑定的元素
-        //binding：binding.value 指令等于号后面绑定的表达式的值 
-        console.log(el,binding.value);
-         useIntersectionObserver(
-          el,
-          ([{ isIntersecting }]) => {
-            console.log(isIntersecting);
-            if (isIntersecting) {
-                el.src = binding.value
-						}
-          },    
-        )
-        
-    },
-})
