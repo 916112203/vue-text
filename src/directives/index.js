7eb168eec474eee6f9cdf69d0c2df07e21ae94eb
zcj <916112203@qq.com>
2023-11-10 17:10:41 +0800

    产品列表实现

--- src/directives/index.js
+++ src/directives/index.js
@@ -6,9 +6,7 @@ export const lazyPlugin = {
 			mounted(el, binding) {
 				//el：指令绑定的元素
 				//binding：binding.value 指令等于号后面绑定的表达式的值
-				console.log(el, binding.value);
 				const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
-					console.log(isIntersecting);
 					if (isIntersecting) {
 						el.src = binding.value;
                         stop()
